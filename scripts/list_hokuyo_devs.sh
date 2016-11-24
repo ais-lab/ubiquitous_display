dbaf25e4d9b8e46db3626ce834e575fe7ffd6e6b
Ryuhei Sakurai <rsakurai@fc.ritsumei.ac.jp>
2016-11-24 16:07:57 +0900

    Hokuyoのデバイスファイル名を列挙するスクリプトを追加

---
+++ scripts/list_hokuyo_devs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Usage: list_hokuyo_devs [options]
+#
+# Options:
+#   -v     Show device name with Hokuyo ID
+#   -h     Show help
+
+getopts "hv" opts
+if [ $opts == h ] ; then
+  sed -rn '/^# Usage/,${/^#/!q;s/^# ?//;p}' "$0"
+  exit 1 
+fi
+
+for dev in `ls /dev/ttyACM*` ; do
+  id=`timeout 1 rosrun hokuyo_node getID $dev -`
+  out_str=$dev
+
+  if [ $opts == v ] ; then
+    out_str="$out_str $id"
+  fi
+
+  if [ -n "$id" ] ; then
+    echo $out_str
+  fi
+done
