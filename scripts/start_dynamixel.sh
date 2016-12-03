d57feba2fa35f132e445ec737cb6a4c42c0b2ee9
gologius <thank.you.notification+registration.com>
2016-12-03 12:39:19 +0900

    add launch file

--- scripts/start_dynamixel.sh
+++
@@ -1,6 +0,0 @@
-roslaunch ubiquitous_display pantilt_dxl_manager.launch &
-sleep 1s
-roslaunch ubiquitous_display pantilt_dxl_controller.launch & 
-sleep 1s
-roslaunch ubiquitous_display pantilt_dxl_joint_state_publisher.launch &
- 
